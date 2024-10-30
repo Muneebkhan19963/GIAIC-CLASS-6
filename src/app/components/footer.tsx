0365b47330a19d9a5c33979577aa149e2fd4cf0f
Muneebkhan19963 <mk347719@gmail.com>
2024-10-30 03:52:00 -0700

    first commit

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='mt-20'>
+        <p className='bg-gradient-to-b from-black to-blue-950 text-white p-3 text-center py-6 font-extrabold'>
+        ♥ Made by Love @<a href="https://www.linkedin.com/in/fariha-faraz-90365b14b" target='blank'> Muneeb Khan</a> ♥
+        </p>
+    </div>
+  )
+}
